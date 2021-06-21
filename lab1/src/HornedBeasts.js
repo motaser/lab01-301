e8b8938aea6f01a9996b39800bd985a6443d6bf8
motaser <araidahmontaser@gmail.com>
2021-06-21 22:29:45 +0300

    creat lab1

---
+++ lab1/src/HornedBeasts.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+ class HornedBeasts extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default HornedBeasts
---
+++ lab1/src/Main.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class Main extends Component {
+    render() {
+        return (
+            <div>
+                <HornedBeasts
+                title=""
+                imageUrl=""
+                description=""
+                
+                />
+            </div>
+        )
+    }
+}
+
+export default Main
