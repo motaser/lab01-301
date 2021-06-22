ea8a54a1c3b66015ae076e5d4eadfcda05818889
motaser <araidahmontaser@gmail.com>
2021-06-22 19:27:31 +0300

    class

---
+++ lab1/src/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+ class Footer extends Component {
+    render() {
+        return (
+            <div>
+              <foorer>
+                  <p> montaser araidah &copy; 2021 </p>
+            </foorer>  
+            </div>
+        )
+    }
+}
+
+export default Footer
